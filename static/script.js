fd71d59db684c318127a79faf9d6b02b1141c976
Jeffrey I. Escobar <Jeffreyianescobar@gmail.com>
2023-09-06 13:42:52 -0400

    added squared and cubed function

--- static/script.js
+++ static/script.js
@@ -1,33 +1,47 @@
 // Function to add a digit or operator to the display
 function addToDisplay(value) {
-    document.getElementById('display').value += value;
+    // Replace symbols for division and multiplication
+    if (value === '/') {
+        value = '÷'; // Replace '/' with '÷'
+    } else if (value === '×') {
+        value = '*'; // Replace '×' with '*'
+    }
+    
+    // Replace '÷' with '/' before adding to the display
+    const display = document.getElementById('display');
+    display.value += value === '÷' ? '/' : value;
 }
+
 
 // Function to calculate the result based on the user input
 function calculate() {
     try {
         const expression = document.getElementById('display').value;
-        let result = eval(expression);
+        const result = eval(expression);
 
-        // Get the selected output format (fraction or decimal)
-        const outputFormat = document.getElementById('output-format').value;
-
-        if (outputFormat === 'fraction') {
-            if (result.denominator === 0) {
-                document.getElementById('result').textContent = 'Division by Zero';
-            } else {
-                result = new Fraction(result).limitDenominator();
-                document.getElementById('result').textContent = `${result.numerator}/${result.denominator}`;
-            }
-        } else {
-            document.getElementById('result').textContent = result;
-        }        
+        document.getElementById('result').textContent = result;
     } catch (error) {
         // Handle any errors (e.g., division by zero) and display an error message
         document.getElementById('result').textContent = 'Error';
     }
 }
+// Function to square the displayed number
+function square() {
+    const expression = document.getElementById('display').value;
+    let number = eval(expression);
 
+    const result = Math.pow(number, 2);
+    document.getElementById('result').textContent = result;
+}
+
+// Function to cube the displayed number
+function cube() {
+    const expression = document.getElementById('display').value;
+    let number = eval(expression);
+
+    const result = Math.pow(number, 3);
+    document.getElementById('result').textContent = result;
+}
 
 // Function to calculate the square root of the displayed number
 function calculateSquareRoot() {
